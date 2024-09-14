cad5a2fae9fc9dd6b2e82f97b01042c95ad8f009
OriQuack <kimdoh0423@naver.com>
2024-09-14 21:34:36 +0900

    renewed authentication middleware

--- Authorization/middleware/auth.js
+++ Authorization/middleware/auth.js
@@ -1,18 +1,36 @@
 const User = require('../models/user');
+const redisUtil = require('../utils/redisUtil');
 
 const authenticate = async (req, res, next) => {
     const userId = req.headers['userId'];
-
-    if (!accessToken) {
-        return res.status(403).json({ message: 'Authentication required: no access token' });
-    }
     const user = await User.getUserByUserId(userId);
     if (!user) {
         return res.status(404).json({ message: 'User not found' });
     }
 
-    req.user = new User(user);
-    return next();
+    const refreshToken = redisUtil.getRefreshToken(userId);
+    if (!refreshToken) {
+        return res.status(403).json({ message: 'RT invalid or expired' });
+    }
+
+    return jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            complete: true,
+            algorithms: ['HS256'],
+            clockTolerance: 0,
+            ignoreExpiration: false,
+            ignoreNotBefore: false,
+        },
+        async (err, refreshDecoded) => {
+            if (err) {
+                return res.status(403).json({ message: 'RT invalid or expired' });
+            }
+            req.user = new User(user);
+            return next();
+        }
+    );
 };
 
 module.exports = authenticate;
