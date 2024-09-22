b106ce235ca1cd53617b34a9333d8c61361837c3
oriquack <kimdoh0423@naver.com>
2024-09-22 14:59:38 +0900

    temporarily updated access token expriation

--- Authorization/utils/tokenUtil.js
+++ Authorization/utils/tokenUtil.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const genAccessToken = (userId) => {
     const payload = { userId: userId };
     return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1h',
+        expiresIn: '1d', // WILL CHANGE
         notBefore: '0',
         algorithm: 'HS256',
         // issuer
